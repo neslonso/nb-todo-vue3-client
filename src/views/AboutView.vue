6783ad084ff36d5f4925792263cb6f4f76875c15
Néstor <nestor@parqueweb.com>
2023-07-06 21:05:55 +0200

    refactor: minimal about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,40 @@
 <template>
-  <div class="about">
-    <h1>TODO List<br /><small>Prueba de concetpo</small></h1>
-    <p>Consideraciones de desarrollo:</p>
-    <ul>
-      <li>
-        Los services de Task y Category existen solo de forma demostativa. Su
-        función sería ofrecer la posibilidad de agregar datos de fuentes
-        adicionales (una API, un archivo, de un socket, etc.). En ese caso:
-        <ul>
-          <li>
-            El constructor de cada service recibiría un array de interfaces a
-            repositorios
-          </li>
-          <li>
-            Cada operación (salvo getAll*) recibiría el identificador de la
-            fuente de datos a utilizar
-          </li>
-        </ul>
-      </li>
-      <li>
-        Los repositories de Task y Category existen para minimizar el
-        acoplamiento con el sistema de persistencia subyancete a cada uno. En
-        caso de necesitar cambiar de sistema de persistencia, solo habría que
-        modificar el repository correspondiente.
-      </li>
-    </ul>
+  <div class="flex flex-col justify-center items-center p-8">
+    <div
+      class="bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col max-w-2xl"
+    >
+      <h1 class="font-bold text-4xl mb-2 text-gray-700">TODO LIST</h1>
+      <h2 class="font-semibold text-xl mb-4 text-gray-600">
+        Prueba de Concepto
+      </h2>
+      <p class="text-gray-700 text-base">
+        Esta es una prueba de concepto para el desarrollo de una aplicación web
+        con Vue3 integrada con Laravel. No pretende ser una aplicación completa
+        y existen características sin implementar y sin depurar.
+      </p>
+      <p class="mt-3">
+        Para su desarrollo se ha utilizado (además de lo obvio):
+      </p>
+      <ul class="pl-16 mt-7 list-disc list-inside text-gray-700">
+        <li class="mb-2">Windows 10 LTSC + WLS2 (Ubuntu)</li>
+        <li class="mb-2">Visual Studio Code</li>
+        <li class="mb-2">Sublime Merge</li>
+        <li class="mb-2">Postman</li>
+        <li class="mb-2">HeidiSQL</li>
+        <li class="mb-2">
+          <span>Proxmox</span>
+          <ul class="pl-7 mt-2 list-disc list-inside text-gray-700">
+            <li class="mb-2">Debian 12</li>
+            <li class="mb-2">Portainer</li>
+          </ul>
+        </li>
+        <li class="mb-2">GPT-4</li>
+      </ul>
+      <p class="text-right font-medium text-gray-700">
+        Made with
+        <span class="text-red-500">❤️</span>
+        by Me
+      </p>
+    </div>
   </div>
 </template>
